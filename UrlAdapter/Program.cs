1e4acd32bfd465f35b0eb06129cbebf290ee1c24
Vincent Neparidze <nva@akmetron.ru>
2017-11-09 11:56:43 +0300

    Initial commit

---
+++ UrlAdapter/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace UrlAdapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (string a in args)
+            {
+                Console.WriteLine(a.Remove(0, a.LastIndexOf("http")).Replace("%3A", ":").Replace("%2F", "/"));
+            }
+        }
+    }
+}
