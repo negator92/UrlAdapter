9d02fcfe9a9683ef5479bfcac1a47938b79f7b25
Vincent Neparidze <nva@akmetron.ru>
2017-11-16 10:15:35 +0300

    Added step to exit

--- UrlAdapter/Program.cs
+++ UrlAdapter/Program.cs
@@ -11,6 +11,8 @@ namespace UrlAdapter
             {
                 Console.WriteLine("Enter url: ");
                 input = Console.ReadLine();
+                if(string.IsNullOrEmpty(input) && string.IsNullOrWhiteSpace(input))
+                    return;
                 int httpLastIndex = input.LastIndexOf("http");
                 Console.WriteLine(input.Substring(httpLastIndex, (input.Contains(".html") ? (input.IndexOf(".html") + 5 - httpLastIndex) : input.Length - httpLastIndex)).Replace("%3A", ":").Replace("%2F", "/"));
                 Console.WriteLine("Press enter to continue or q to quit");
