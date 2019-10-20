4b35f56dbf09ba53770a5c04c11410fc9faf88f5
Vincent Neparidze <negator92@gmail.com>
2019-10-20 16:49:14 +0300

    Refactoring code

--- UrlAdapter/Program.cs
+++ UrlAdapter/Program.cs
@@ -4,17 +4,20 @@ namespace UrlAdapter
 {
     internal class Program
     {
-        static void Main()
+        private static void Main()
         {
-            string input;
             do
             {
                 Console.WriteLine("Enter url: ");
-                input = Console.ReadLine();
-                if(string.IsNullOrEmpty(input) && string.IsNullOrWhiteSpace(input))
+                var input = Console.ReadLine();
+                if (string.IsNullOrEmpty(input) && string.IsNullOrWhiteSpace(input))
                     return;
-                int httpLastIndex = input.LastIndexOf("http");
-                Console.WriteLine(input.Substring(httpLastIndex, (input.Contains(".html") ? (input.IndexOf(".html") + 5 - httpLastIndex) : input.Length - httpLastIndex)).Replace("%3A", ":").Replace("%2F", "/"));
+                var httpLastIndex = input.LastIndexOf("http");
+                Console.WriteLine(input
+                    .Substring(httpLastIndex,
+                        (input.Contains(".html")
+                            ? (input.IndexOf(".html") + 5 - httpLastIndex)
+                            : input.Length - httpLastIndex)).Replace("%3A", ":").Replace("%2F", "/"));
                 Console.WriteLine("Press enter to continue or q to quit");
             } while (Console.ReadLine() != "q");
         }
